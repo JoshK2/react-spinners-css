0b9f50104d5032df55df8cef24ff8e5489455f5f
José Cabeda <jecabeda@gmail.com>
2019-10-05 11:48:27 +0100

    Added ouroboro

---
+++ src/components/Ouroboro/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './style.css'
+
+export default function Ouroboro({ color, className, style }) {
+    return (
+        <div className="circle" >
+            <div className="spinner" >
+                <span className="left" >
+                    <span className="anim" ></span>
+                </span>
+                <span className="right" >
+                    <span clasName="anim"></span>
+                </span>
+            </div>
+        </div>
+    )
+}
+
+Ouroboro.propTypes = {
+    /** hex color */
+    color: PropTypes.string,
+    /** class name  */
+    className: PropTypes.string,
+    /** style object */
+    style: PropTypes.object,
+}
+
+Ouroboro.defaultProps = {
+    color: '#7f58af',
+    className: '',
+    style: {},
+}
